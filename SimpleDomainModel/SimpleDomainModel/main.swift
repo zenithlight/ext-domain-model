abef9df6141dc7336475c8c25341b7c21bb0c52f
Ted Neward <ted@tedneward.com>
2016-04-06 16:59:37 -0700

    Basic project scaffold structure in place

---
+++ SimpleDomainModel/SimpleDomainModel/main.swift
@@ -0,0 +1,15 @@
+//
+//  main.swift
+//  SimpleDomainModel
+//
+//  Created by Ted Neward on 4/6/16.
+//  Copyright © 2016 Ted Neward. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
+public func testMe() -> String {
+  return "I have been tested"
+}
